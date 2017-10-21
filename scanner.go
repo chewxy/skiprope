3bd382c13b25454768adef3a9feaf4cdaf9fa76f
chewxy <chewxy@gmail.com>
2017-10-21 15:09:38 +1100

    ADded io.Reader, io.ByteReader and tests

--- scanner.go
+++ scanner.go
@@ -10,9 +10,10 @@ var ErrSOF = errors.New("Cannot unread on SOF")
 
 // Scanner is a linear scanner over a *Rope which returns runes.
 type Scanner struct {
-	skiplist
-	k      *knot // current
-	offset int
+	*Rope
+	k         *knot // current
+	offset    int
+	readBytes int // how many bytes has been read
 
 	// lastSize is used in implementing UnreadRune()
 	lastSize int
@@ -21,29 +22,83 @@ type Scanner struct {
 
 // NewScanner creates a new scanner.
 func NewScanner(r *Rope) *Scanner {
-	s := &Scanner{
-		skiplist: skiplist{r: r},
-	}
+	sl := skiplist{r: r}
+	s := &Scanner{Rope: r}
 	var err error
-	if s.k, s.offset, _, err = s.find(0); err != nil {
+	if s.k, s.offset, _, err = sl.find(0); err != nil {
 		panic(err)
 	}
-	return s
-}
 
-// ReadRune implements io.RuneReader. It reads a single UTF8 encoded character and then returns its size in bytes
-func (s *Scanner) ReadRune() (rune, int, error) {
-	if s.k == nil {
-		return -1, -1, io.EOF
-	}
-	if s.k.used-s.offset <= 0 {
+	// first block may not be used
+	for s.k.used-s.offset <= 0 {
 		// go to next block
 		s.prevK = s.k
 		s.k = s.k.nexts[0].knot
 	}
+	return s
+}
+
+// Len returns the number of bytes unread
+func (s *Scanner) Len() int { return s.size - s.readBytes }
+
+// Read implements io.Reader. It reads up to len(p) bytes into p. The Scanner is a stateful reader,
+// meaning it will keep track of how many bytes has been read.
+func (s *Scanner) Read(p []byte) (n int, err error) {
+	if s.readBytes >= s.size || s.k == nil {
+		return 0, io.EOF
+	}
+
+	l := len(p)
+	used := s.k.used - s.offset
+	n = copy(p, s.k.data[s.offset:s.k.used])
+	if l <= used {
+		s.offset += n
+		s.readBytes += n
+		return
+	}
+
+	// TODO: recursive reading is simple to understand but performance is ??? .
+	remainder := l - n
+	if remainder > 0 {
+		s.offset = 0
+
+		var n2 int
+		s.k = s.k.nexts[0].knot
+		if n2, err = s.Read(p[remainder:]); err != nil && err != io.EOF {
+			return n, err
+		}
+		err = nil
+		n += n2
+	}
+	return
+}
+
+// ReadByte  implements io.ByteReader
+func (s *Scanner) ReadByte() (byte, error) {
+	if s.readBytes >= s.size || s.k == nil {
+		return 0, io.EOF
+	}
+	retVal := s.k.data[s.offset]
+	s.offset++
+	s.readBytes++
+	if s.offset >= s.k.used {
+		s.prevK = s.k
+		s.k = s.k.nexts[0].knot
+		s.offset = 0
+	}
+	return retVal, nil
+}
+
+// ReadRune implements io.RuneReader. It reads a single UTF8 encoded character and then returns its size in bytes
+func (s *Scanner) ReadRune() (rune, int, error) {
+	if s.k == nil || s.readBytes >= s.size {
+		return -1, -1, io.EOF
+	}
+
 	r, size := utf8.DecodeRune(s.k.data[s.offset:s.k.used])
 	s.lastSize = size
 	s.offset += size
+	s.readBytes += size
 	if s.offset >= s.k.used {
 		s.prevK = s.k
 		s.k = s.k.nexts[0].knot
@@ -57,6 +112,7 @@ func (s *Scanner) UnreadRune() error {
 	if s.offset == 0 && s.prevK == nil {
 		return ErrSOF
 	}
+	s.readBytes -= s.lastSize
 	if s.offset == 0 {
 		s.k = s.prevK
 		s.prevK = nil
--- scanner_test.go
+++ scanner_test.go
@@ -147,5 +147,67 @@ func TestScanner_UnreadRune(t *testing.T) {
 	if err != ErrSOF {
 		t.Error(err)
 	}
+}
 
+func TestScanner_Read(t *testing.T) {
+	r := New()
+	r.Insert(0, "This is a  long string that is meant to span multiple *knots")
+
+	p := make([]byte, 5)
+	s := NewScanner(r)
+	n, err := s.Read(p)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n != 5 {
+		t.Error("Expected a read of 5 bytes")
+	}
+	if string(p) != "This " {
+		t.Error("First read failed")
+	}
+	n, err = s.Read(p)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(p) != "is a " {
+		t.Error("2nd read failed")
+	}
+
+	// Longer stuff
+	s = NewScanner(r)
+	p = make([]byte, 1024)
+	if n, err = s.Read(p); err != nil {
+		t.Fatal(err)
+	}
+	if string(p[:n]) != "This is a  long string that is meant to span multiple *knots" {
+		t.Error("Long read failed")
+	}
+}
+
+// ReadByte is affected by other method calls, as demonstrated by caling ReadRune
+func ExampleScanner_ReadByte() {
+	r := New()
+	r.Insert(0, "Hello World")
+
+	s := NewScanner(r)
+	fmt.Printf("String Size: %d.\n", len("Hello World"))
+	s.ReadByte() // H
+	s.ReadRune() // E
+	fmt.Printf("Bytes Unread: %d\n", s.Len())
+	for b, err := s.ReadByte(); err == nil; b, err = s.ReadByte() {
+		fmt.Printf("%q\n", b)
+	}
+
+	// Output:
+	// String Size: 11.
+	// Bytes Unread: 9
+	// 'l'
+	// 'l'
+	// 'o'
+	// ' '
+	// 'W'
+	// 'o'
+	// 'r'
+	// 'l'
+	// 'd'
 }
